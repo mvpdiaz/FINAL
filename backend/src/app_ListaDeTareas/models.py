b636f2b395f89f650f551e0f2796719d005e0b35
ismaelg84 <franjain.ig@gmail.com>
2023-09-12 16:22:16 -0300

    FIX: cambio en model.py, ForeignKey

--- backend/src/app_ListaDeTareas/models.py
+++ backend/src/app_ListaDeTareas/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 from users.models import UserAccount
+from django.conf import settings
 
 # Create your models here.
 class ListaTareas(models.Model):
     nombreTarea=models.CharField(max_length=255)
     fechaCreacionTarea=models.DateField(auto_now_add=True) #2023-09-05
-    idUsario=models.ForeignKey(UserAccount, on_delete=models.CASCADE) #hay que hacer una foranea al Usuario
+    idUsuario=models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE) #hay que hacer una foranea al Usuario
